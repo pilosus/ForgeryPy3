7f8a033690a5b727ac1f787a913908caf55dc9a1
Vitaly R. Samigullin <vrs@pilosus.org>
2016-11-15 22:55:31 +0300

    Password salt for hashlib.sha1() fixed

--- forgery_py/forgery/basic.py
+++ forgery_py/forgery/basic.py
@@ -119,7 +119,7 @@ def encrypt(password='password', salt=None):
         # see https://pymotw.com/2/hashlib/
         # see https://docs.python.org/release/2.5/lib/module-hashlib.html
         dk = hashlib.sha1()
-        dk.update(password.encode())
+        dk.update(password.encode() + salt.encode())
         hexdigest = dk.hexdigest()
     return hexdigest
 
