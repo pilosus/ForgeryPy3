839ac645ebec0e2fcaf05bfc232e72ce7fb28967
Vitaly R. Samigullin <vrs@pilosus.org>
2016-11-15 22:52:05 +0300

    hashlib.sha1() use fixed

--- forgery_py/forgery/basic.py
+++ forgery_py/forgery/basic.py
@@ -114,13 +114,14 @@ def encrypt(password='password', salt=None):
     try:
         #  available for python 2.7.8 and python 3.4+
         dk = hashlib.pbkdf2_hmac('sha1', password.encode(), salt.encode(), 100000)
+        hexdigest = binascii.hexlify(dk).decode('utf-8')
     except AttributeError:
         # see https://pymotw.com/2/hashlib/
         # see https://docs.python.org/release/2.5/lib/module-hashlib.html
         dk = hashlib.sha1()
-        dk.update(password.encode()).hexdigest()
-    binascii.hexlify(dk)
-    return binascii.hexlify(dk).decode('utf-8')
+        dk.update(password.encode())
+        hexdigest = dk.hexdigest()
+    return hexdigest
 
 
 def frequency():
