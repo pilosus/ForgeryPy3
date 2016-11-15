2706b77ad6ecbdac733f2eaff4c53c15fd2ef386
Vitaly R. Samigullin <vrs@pilosus.org>
2016-11-15 22:38:39 +0300

    Python 2.6-, 3.2, 3.3 compatibility hopefully fixed for hashlib sha1.

--- forgery_py/forgery/basic.py
+++ forgery_py/forgery/basic.py
@@ -105,11 +105,20 @@ def color():
 def encrypt(password='password', salt=None):
     """
     Return SHA1 hexdigest of a password (optionally salted with a string).
+
+
     """
     if not salt:
         salt = str(datetime.utcnow())
 
-    dk = hashlib.pbkdf2_hmac('sha1', password.encode(), salt.encode(), 100000)
+    try:
+        #  available for python 2.7.8 and python 3.4+
+        dk = hashlib.pbkdf2_hmac('sha1', password.encode(), salt.encode(), 100000)
+    except AttributeError:
+        # see https://pymotw.com/2/hashlib/
+        # see https://docs.python.org/release/2.5/lib/module-hashlib.html
+        dk = hashlib.sha1()
+        dk.update(password).hexdigest()
     binascii.hexlify(dk)
     return binascii.hexlify(dk).decode('utf-8')
 
